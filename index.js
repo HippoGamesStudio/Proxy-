70c8c21e21c0aa256be921aa83de7cb17bffded6
Hippo <robloxmansuper50@gmail.com>
2025-06-16 11:32:24 +0700

    index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post('/chat', async (req, res) => {
         const userMessage = req.body.message;
 
         const response = await axios.post('https://api.groq.com/openai/v1/chat/completions', {
-            model: "mllama3-8b-8192",
+            model: "llama3-8b-8192",
             messages: [
                 {role: "user", content: userMessage}
             ]
